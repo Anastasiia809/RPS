650366a79d140e36a063cfc11338e143df4ee093
Anastasiia809 <anastasiia208805@student-schoolsofwestfield.org>
2025-05-06 14:56:59 +0000

    almost

--- script.js
+++ script.js
@@ -11,17 +11,58 @@ function main() {
     let winner = findWinner(u,c);
     alert("You chose " + u + " , and I chose " + c + " . " + winner + " won");
 }
+// userturn
+// user can choose r, p, or s.
+// if bad Input, give new choice
+// @param:none
+// @return:choice
 function userTurn() {
     let choice = prompt("enter r, p, or s");
-    if (choice != "r" && choice != "p" && choice != "s") {
+    const turn = ["r","p","s"];
+    if (!turn.includes(choice)) {
         alert("Invalid Input");
         return userTurn();
     }
     return choice;
 }
+// cpuTurn
+// computer choose between r, p, or s
+// @param:none
+// @return: choice
 function cpuTurn() {
     let choice = Math.floor(Math.random()*3);
     let moves = ["r","p","s"];
     return moves[choice];
 }
-function
+// findWinner
+//  takes user and computer turn
+// decides who the winner is
+// returns winner
+// @param:u,c
+// @return: winner
+function findWinner(u,c) {
+    let combo = u + c;
+    switch (combo) {
+      case "rp":
+        winner = "p"
+        break;
+      case "rs":
+        winner = "c"
+        break;
+      case "rr":
+            winner = "none"
+        break;
+      case "ps":
+        winner = "p"
+        break;
+      case "pr":
+        winner = "c"
+        break;
+      case "pp":
+        winner = "none"
+        break;
+      case ""        
+    }
+        alert("You chose " + u + " , and I chose " + c + winner + " won");
+        return winner;
+}
